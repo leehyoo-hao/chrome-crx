87d62af47b062eb06bbf88af7c03ab4d0879bd14
lihao <lihao@zhoupudata.com>
2021-10-14 00:33:43 +0800

    content

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { List, Avatar } from 'antd';
+import './popup.styl'
 
 const data = [
   {
@@ -15,9 +16,21 @@ const data = [
     title: 'Ant Design Title 4',
   },
 ];
+
+const header = () => {
+  // todo 获取域名
+  return <div>header</div>;
+}
+
+const footer = () => {
+  return <div>footer</div>;
+}
 const Popup: React.FC = () => {
+  
     return (
-        <List
+      <div className="leehyoo-popup">
+        <List header={header()}
+        footer={footer()}
     itemLayout="horizontal"
     dataSource={data}
     renderItem={item => (
@@ -28,8 +41,9 @@ const Popup: React.FC = () => {
           description="Ant Design, a design language for background applications, is refined by Ant UED Team"
         />
       </List.Item>
+      
     )}
-  />
+  /></div>
     )
 }
 
